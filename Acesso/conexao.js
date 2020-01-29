ee1d60c9a550c7bc85848e9b65ed84871ba31aa6
SIGGA\emanuel.martins <emanuel.martins@sigga.com>
2020-01-29 17:30:59 -0300

    ajustes nas mensagens de repasse da função comando

--- Acesso/conexao.js
+++ Acesso/conexao.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql')
+
 const connection = mysql.createConnection({
   host: '127.0.0.1', port: 3306, user: 'root', password: '123456', database: 'dbchemical'
 });
@@ -12,19 +13,27 @@ let consulta = function consulta(sqlQry, params, res) {
     if (error)
       console.log(error);
     else {
+      console.log(results)
       res.json(results);
     }
   });
 }
 
-let comando = function comando(sqlQry, params, ret) {
-  connection.query(sqlQry, params, function (error, results, fields) {
-    if (error)
-      console.log(error);
-    else {
-      console.log(results.insertId)
-      ret = results.insertId;
-    }
+let comando = async function (sqlQry, params) {
+  return new Promise((resolve, reject) => {
+    connection.query(sqlQry, params, function (error, results, fields) {
+      if (error) {
+        console.log(error);
+        if (error.code == "ER_DUP_ENTRY") {
+          resolve([1, "Erro: Dados já cadastrados!"]);
+        }
+        else
+          resolve([1,"Erro ao tentar executar a tarefa."]);
+      }
+      else {
+        resolve([0,results.insertId]);
+      }
+    })
   })
 }
 
--- Api/index.js
+++ Api/index.js
@@ -18,15 +18,14 @@ app.listen(port, function () {
 
 app.get('/elementosws/:Id', (req, res) => {
   if (req.params.Id == 0) {
-    con.consulta("select id,nome,simbolo,numero,estado,descricao from elementoquimico", null, res);
+    con.consulta("select id,nome,simbolo,numero,estado,descricao from elementoquimico order by numero", null, res);
   }
   else {
-    con.consulta("select id,nome,simbolo,numero,estado,descricao from elementoquimico where id = ?", req.params.Id, res);
+    con.consulta("select id,nome,simbolo,numero,estado,descricao from elementoquimico where id = ? order by numero", req.params.Id, res);
   }
 })
 
 app.get('/formulaws/:Id', (req, res) => {
-
   querie = "select eq.nome,eq.simbolo,fq.quantidade,fq.id,fq.idelemento from formulaquimica fq inner join elementoquimico eq on fq.idelemento = eq.id";
   if (req.params.Id == 0) {
     con.consulta(querie, null, res)
@@ -45,96 +44,75 @@ app.get('/compostows/:Id', (req, res) => {
 })
 
 app.post('/cadelementows', (req, res) => {
-
-  var resp;
-
   if (req.body.Id > 0) {
-    params = [req.body.Nome, req.body.Simbolo, req.body.Numero, req.body.Estado, req.body.Descricao, req.body.Id]
-    console.log(params)
-    con.comando("update elementoquimico set nome = ?, simbolo = ?,numero= ?,estado = ?,descricao = ? where id = ?", params, resp);
+    params = 
+      (con.comando("update elementoquimico set nome = ?, simbolo = ?,numero= ?,estado = ?,descricao = ? where id = ?",
+       [req.body.Nome, req.body.Simbolo, req.body.Numero, req.body.Estado, req.body.Descricao, req.body.Id])).then(function (result) {
+        ConstroiResposta(res, result)
+      });
   } else {
-    params = [req.body.Nome, req.body.Simbolo, req.body.Numero, req.body.Estado, req.body.Descricao]
-    con.comando("insert into elementoquimico(nome,simbolo,numero,estado,descricao) values (?,?,?,?,?);", params, resp);
+    
+      (con.comando("insert into elementoquimico(nome,simbolo,numero,estado,descricao) values (?,?,?,?,?);", 
+      [req.body.Nome, req.body.Simbolo, req.body.Numero, req.body.Estado, req.body.Descricao])).then(function (result) {
+        ConstroiResposta(res, result)
+      });
   }
-
-  console.log(resp);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader('Content-Type', 'application/json');
-  res.json(resp);
-
 })
 
 app.post('/cadcompostows', (req, res) => {
-  var resp;
+ console.log(req.body)
   if (req.body.Id > 0) {
-    params = [req.body.Composto, req.body.Aparencia, req.body.Id]
-    console.log(params)
-    con.comando("update compostoquimico set nome = ?,aparencia = ? where id = ?", params, resp);
+      (con.comando("update compostoquimico set nome = ?,aparencia = ? where id = ?", [req.body.Composto, req.body.Aparencia, req.body.Id])).then(function (result) {
+        ConstroiResposta(res, result)
+        console.log(result)
+      });
   } else {
-    params = [req.body.Composto, req.body.Aparencia]
-    con.comando("insert into compostoquimico(nome,aparencia) values(?,?);", params, resp);
+    (con.comando("insert into compostoquimico(nome,aparencia) values(?,?);", [req.body.Composto, req.body.Aparencia])).then(function (result) {
+      ConstroiResposta(res, result)
+    });
   }
-
-  console.log(resp);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader('Content-Type', 'application/json');
-  res.json(resp);
-
 })
 
 app.post('/cadformulaws', (req, res) => {
-  var resp;
   if (req.body.Id > 0) {
-    params = [req.body.Idelemento, req.body.Quantidade, req.body.IdComposto, req.body.Id]
-    console.log(params)
-    con.comando("update formulaquimica set idelemento = ?, quantidade = ?,idcomposto= ? where id = ?;", params, resp);
+      (con.comando("update formulaquimica set idelemento = ?, quantidade = ?,idcomposto= ? where id = ?;", [req.body.Idelemento, req.body.Quantidade, req.body.IdComposto, req.body.Id])).then(function (result) {
+        ConstroiResposta(res, result)
+      });
   } else {
-    params = [req.body.Idelemento, req.body.Quantidade, req.body.IdComposto]
-    con.comando("insert into formulaquimica(idelemento,quantidade,idcomposto) values(?,?,?); ", params, resp);
+      (con.comando("insert into formulaquimica(idelemento,quantidade,idcomposto) values(?,?,?);", [req.body.Idelemento, req.body.Quantidade, req.body.IdComposto])).then(function (result) {
+        ConstroiResposta(res, result)
+      });
   }
-
-  console.log(resp);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader('Content-Type', 'application/json');
-  res.json(resp);
-
 })
 
 app.get('/delelementows/:Id', (req, res) => {
-  console.log(req.params.Id)
-  var resp
-  con.comando("delete from elementoquimico where id = ?", req.params.Id, resp)
-  console.log(resp);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader('Content-Type', 'application/json');
-  res.json(resp);
+  (con.comando("delete from elementoquimico where id = ?", req.params.Id)).then(function (result) {
+    ConstroiResposta(res, result)
+  });
 })
 
 app.get('/delformulaws/:Id', (req, res) => {
-  var resp;
-  con.comando("delete from formulaquimica where id = ?", req.params.Id, resp)
-  console.log(resp);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader('Content-Type', 'application/json');
-  res.json(resp);
+  (con.comando("delete from formulaquimica where id = ?", req.params.Id)).then(function (result) {
+    ConstroiResposta(res, result)
+  });
 })
 
 app.get('/delcompostows/:Id', (req, res) => {
-  var resp;
-  con.comando("delete from formulaquimica where idcomposto = ?", req.params.Id, resp)
-  con.comando("delete from compostoquimico where id = ?", req.params.Id, resp)
-
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader('Content-Type', 'application/json');
-  res.json(resp);
-  console.log(resp);
+  (con.comando("delete from formulaquimica where idcomposto = ?", req.params.Id)).then(function (result) {
+    if (result => 0) {
+      (con.comando("delete from compostoquimico where id = ?", req.params.Id)).then(function (result) {
+        ConstroiResposta(res, result)
+      });
+    }
+  });
 })
+
+function ConstroiResposta(res, result) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify(result))
+}
 
 
 
